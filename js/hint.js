7fb1c3c41568bbc0f25bb13886b9d5017fa142df
Uri Gross <urigross@gmail.com>
2021-05-20 20:34:58 +0300

    Add files via upload
    another img src fix

--- js/hint.js
+++ js/hint.js
@@ -5,7 +5,7 @@ var gHintsLeft = 3;
 function bulbOn(bulbNum) {
     gGame.isHintOn = true;
     var ele = document.querySelector(`.bulb-${bulbNum}`);
-    ele.src = "/img/bulb-light.jpg";
+    ele.src = "img/bulb-light.jpg";
     ele.className = `.bulb-${bulbNum}-on`;
     gHintsLeft--;
 
